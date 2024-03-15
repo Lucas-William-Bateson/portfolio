8c2d4451fe93cf64e469a5db426b14cba79c43db
Lucas Bateson <lucas.bateson@gmail.com>
2024-03-15 11:55:53 +0100

    Add mobile view, and better spacing

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        'over': '0 -15px 60px -15px rgba(0, 0, 0, 0.3)',
+      }
+    }
   },
   plugins: [],
 }
